0a0330c12342873c3a1aac31a9e6129a1a47e257
ropoko <rodrigostramantinoli@gmail.com>
2023-06-01 18:51:07 -0300

    fix: tsconfig

--- src/__tests__/LoginForm.test.tsx
+++ src/__tests__/LoginForm.test.tsx
@@ -1,56 +1,58 @@
 import { fireEvent, render } from "@testing-library/react";
 
-import LoginForm, { Props } from "../components/LoginForm";
+import LoginForm, { Props } from "./../components/LoginForm";
 
 const renderLoginForm = (props: Partial<Props> = {}) => {
-    const defaultProps: Props = {
-      onPasswordChange() {
-        return;
-      },
-      onRememberChange() {
-        return;
-      },
-      onUsernameChange() {
-        return;
-      },
-      onSubmit() {
-        return;
-      },
-      shouldRemember: true
-    };
-    return render(<LoginForm {...defaultProps} {...props} />);
-  }
+    
+  const defaultProps: Props = {
+    onPasswordChange() {
+      return;
+    },
+    onRememberChange() {
+      return;
+    },
+    onUsernameChange() {
+      return;
+    },
+    onSubmit() {
+      return;
+    },
+    shouldRemember: true
+  };
+
+  return render(<LoginForm {...defaultProps} {...props} />);
+}
 
 describe("<LoginForm />", () => {
-    test("should display a blank login form, with remember me checked by default", async () => {
-        const { findByTestId } = renderLoginForm();
-      
-        const loginForm = await findByTestId("login-form");
-      
-        expect(loginForm).toHaveFormValues({
-          username: "",
-          password: "",
-          remember: true
-        });
+  test("should display a blank login form, with remember me checked by default", async () => {
+      const { findByTestId } = renderLoginForm();
+    
+      const loginForm = await findByTestId("login-form");
+    
+      expect(loginForm).toHaveFormValues({
+        username: "",
+        password: "",
+        remember: true
       });
+    });
 
-      test("should allow entering a username", async () => {
-        const onUsernameChange = jest.fn();
-        const { findByTestId } = renderLoginForm({ onUsernameChange });
-        const username = await findByTestId("username");
-      
-        fireEvent.change(username, { target: { value: "test" } });
-      
-        expect(onUsernameChange).toHaveBeenCalledWith("test");
-      });
-      
-      test("should allow entering a password", async () => {
-        const onPasswordChange = jest.fn();
-        const { findByTestId } = renderLoginForm({ onPasswordChange });
-        const username = await findByTestId("password");
-      
-        fireEvent.change(username, { target: { value: "password" } });
-      
-        expect(onPasswordChange).toHaveBeenCalledWith("password");
-      });
+    test("should allow entering a username", async () => {
+      const onUsernameChange = jest.fn();
+      const { findByTestId } = renderLoginForm({ onUsernameChange });
+      const username = await findByTestId("username");
+    
+      fireEvent.change(username, { target: { value: "test" } });
+    
+      expect(onUsernameChange).toHaveBeenCalledWith("test");
+    });
+    
+    test("should allow entering a password", async () => {
+      const onPasswordChange = jest.fn();
+      const { findByTestId } = renderLoginForm({ onPasswordChange });
+      const username = await findByTestId("password");
+    
+      fireEvent.change(username, { target: { value: "password" } });
+    
+      expect(onPasswordChange).toHaveBeenCalledWith("password");
+    });
 });
